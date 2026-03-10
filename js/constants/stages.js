a470600c8274ff9f55537f9caf11f0fc50204f32
lis0401night <lis0401night@gmail.com>
2026-03-10 21:33:12 +0900

    ステージBGMを変更

--- js/constants/stages.js
+++ js/constants/stages.js
@@ -2,11 +2,11 @@
  * Mini Card Battle - Stage Data
  */
 const STAGES = {
-    android: { id: 'android', name: 'セクター7' },
-    dragon: { id: 'dragon', name: '活火山' },
-    knight: { id: 'knight', name: '戦場' },
-    cthulhu: { id: 'cthulhu', name: '狂気の深淵' },
-    elf: { id: 'elf', name: '迷いの森' },
-    cleric: { id: 'cleric', name: '静寂の聖堂' },
-    satan: { id: 'satan', name: '魔王城' }
+    android: { id: 'android', name: 'セクター7', bgm: 'bgmStageAndroid' },
+    dragon: { id: 'dragon', name: '活火山', bgm: 'bgmBattle' },
+    knight: { id: 'knight', name: '戦場', bgm: 'bgmBattle' },
+    cthulhu: { id: 'cthulhu', name: '狂気の深淵', bgm: 'bgmBattle' },
+    elf: { id: 'elf', name: '迷いの森', bgm: 'bgmBattle' },
+    cleric: { id: 'cleric', name: '静寂の聖堂', bgm: 'bgmBattle' },
+    satan: { id: 'satan', name: '魔王城', bgm: 'bgmBattle' }
 };
