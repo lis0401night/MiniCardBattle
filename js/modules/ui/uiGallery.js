7d3af0528fc7e15fbf7908bce3140b71e0c7c825
lis0401night <lis0401night@gmail.com>
2026-03-15 21:36:16 +0900

    ファイアドラゴンのプレミアムカードを追加

--- js/modules/ui/uiGallery.js
+++ js/modules/ui/uiGallery.js
@@ -87,8 +87,8 @@ function debugUnlockCards() {
             }
         });
         
-        // プレミアムカード(empress, assassin, cyberdragoon)の解放
-        const premiumTargets = ['empress', 'assassin', 'cyberdragoon'];
+        // プレミアムカード(empress, assassin, cyberdragoon, dragon)の解放
+        const premiumTargets = ['empress', 'assassin', 'cyberdragoon', 'dragon'];
         premiumTargets.forEach(id => {
             if (!unlockedPremiumCards.includes(id)) {
                 unlockedPremiumCards.push(id);
