27afe845bad1bc9ef828269ae0d0afe14acae6e2
nae1215 <nae1215@example.com>
2026-03-21 03:07:43 +0900

    3/20 マリア追加 大二郎

--- src/utils/constants/characters.js
+++ src/utils/constants/characters.js
@@ -90,7 +90,7 @@ export const CHARACTERS = {
         easeOfUse: 3,
         filter: 'none', cardType: 'set5', cardBg: 'bg1',
         image: 'assets/characters/char_knight.png', imageLose: 'assets/characters/char_knight_lose.png', icon: 'assets/icons/icon_knight.png', imageEnding: 'assets/characters/char_knight_ending.png', color: '#facc15',
-        leaderSkill: { name: '聖なる進軍', desc: '(SP:5) 自分のレーンに「騎士(P:2)」を最大2体召喚し、自分の場のすべてのカードのパワーを+2する。', cost: 5, action: 'holy_march' },
+        leaderSkill: { name: '聖なる進軍', desc: '(SP:5) 自分のレーンに「騎士(P:1)」を最大2体召喚し、自分の場のすべてのカードのパワーを+2する。', cost: 5, action: 'holy_march' },
         preBattleLine: '我が剣にかけて、勝利を誓おう！',
         mirrorIntro: '私の影……。迷いがあるというのか。だが止まってはいられない。魔王サタンを討ち、この世界に光を取り戻す。それが私の使命だ！',
         narratorIntro: '聖騎士団を率いる若き天才剣士、セレスティア。その白銀の鎧は、王国の人々の希望の象徴であった。',
