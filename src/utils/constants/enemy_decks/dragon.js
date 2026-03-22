3159a1c6acb4dcf2def516071b92a51c766a9303
nae1215 <nae1215@example.com>
2026-03-22 13:23:56 +0900

    3/22 ブロンズ追加 大二郎

--- src/utils/constants/enemy_decks/dragon.js
+++ src/utils/constants/enemy_decks/dragon.js
@@ -6,7 +6,7 @@ export default {
         'dancer', 'dancer', 'dancer', 'dancer',
         'dinosaur', 'dinosaur',
         'cheetah', 'cheetah', 'cheetah',
-        'airship', 'airship', 'airship',
+        'chameleon', 'chameleon', 'chameleon',
 
     ],
     normal: [
